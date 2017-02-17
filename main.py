45750459b58a8525b0f0f50e8abd28d92388f631
Matt Walther <matt@mashio.net>
2017-02-16 17:40:42 -0800

    Syntax fix

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ with open ( filename ) as file:
     content = file.readlines()
 content = [ line.strip() for line in content ]
 
-while ( arg = content.pop() ):
+arg = content.pop()
+while ( len( content ) != 0 ):
     os.system( "git checkout " + arg )
-    print( "git checkout " + arg )
     input( "Press enter to continue..." )
+    arg = content.pop()
 
 print( "IT'S OVER!" )
